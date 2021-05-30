2f5d503bdc2864cf4665964e332516093decd818
Asprofil <adober.demchenko@gmail.com>
2021-05-30 13:03:56 +0300

    parrent and doctor working

--- server/src/modules/posts/post.model.js
+++ server/src/modules/posts/post.model.js
@@ -1,29 +1,27 @@
 import mongoose, { Schema } from 'mongoose';
-   import slug from   'slug';
    import   uniqueValidator from 'mongoose-unique-validator';
    
    const   PostSchema = new   Schema({
      title: {
        type: String,
        trim: true,
-       required: [true, 'Title   is required!'],
-       minlength: [3, 'Title   need to be longer!'],
-       unique: true,
+       default:null,
      },
      parentid:{
-      type: Schema.Types.ObjectId,
-      ref: 'Post',
+        type:String,
+        trim:true,
+        default:null,
      },
+     doctorid:{
+      type:String,
+      trim:true,
+      default:null,
+   },
      text: {
        type: String,
        trim: true,
        required: [true, 'Text   is required!'],
        minlength: [10, 'Text   need to be longer!'],
-     },
-     slug: {
-       type: String,
-       trim: true,
-       lowercase: true,
      },
      user: {
        type: Schema.Types.ObjectId,
@@ -40,24 +38,19 @@ import mongoose, { Schema } from 'mongoose';
    });
    
    PostSchema.pre('validate', function (next) {
-     this._slugify();
-   
      next();
    });
    
    PostSchema.methods = {
-    _slugify() {
-      this.slug = slug(this.title);
-    },
     toJSON() {
       return {
         _id: this._id,
         title: this.title,
         text: this.text,
-        createdAt: this.createdAt,
-        slug: this.slug,
-        user: this.user,
         parentid:this.parentid,
+        createdAt: this.createdAt,
+        user: this.user,
+        doctorid:this.doctorid,
         favoriteCount: this.favoriteCount,
       };
     },
