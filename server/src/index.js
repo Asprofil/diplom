fd99cdb160e78dc6ccad20354e8f24fd03bbd526
oleg_demchenko <oleg.demchenko@pm.bet>
2021-05-30 20:56:16 +0300

    Added swagger ui, path and support of static directories(files) support in the route

--- server/src/index.js
+++ server/src/index.js
@@ -6,14 +6,35 @@ import middlewaresConfig from './config/middleware';
 import apiRoutes from './modules';
 const app = express();
 middlewaresConfig(app);
-app.get('/', (req, res) => {
-    res.send('Hello world!');
+
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('../swagger.json');
+app.use('/', express.static('dist/public'));
+const resolve = require('path').resolve
+app.get('/', function(req, res){
+
+    // save html files in the `views` folder...
+    res.sendFile(resolve('dist/public/index.html'));
 });
+// app.get('/', (req, res) => {
+//    res.send('Hello world!');
+// });
 apiRoutes(app);
+
+
 app.listen(constants.PORT, err => {
     if (err) {
         throw err;
     } else {
         console.log(` Server running on port: ${constants.PORT} --- Running on ${process.env.NODE_ENV} --- Make something great `);
+        console.log(` Dirname: ${resolve('dist/public/index.html')} `);               
     }
 });
+app.use(
+    '/api-docs',
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerDocument)
+  );
+const path = require('path');
+app.use("/", express.static(__dirname));
+app.use("/public", express.static(path.join(__dirname, 'public')));
